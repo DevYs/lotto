cf9b01e814976bf2fbb0f85b309890aa38b0ce25
한영수 <hanys1042@gmail.com>
2019-04-10 15:02:23 +0900

    당첨 번호 카운팅 기능 추가

--- js/lotto.js
+++ js/lotto.js
@@ -1,5 +1,6 @@
 var MAX_SIZE = 100;
 var MAX_SIZE_EXCESS_MSG = '1회당 구매할 수 있는 최대한도는 ' + MAX_SIZE +'장입니다.';
+var countWin = [];
 var winList;
 var randomNumber = {
     min : 1,
@@ -31,7 +32,8 @@ var randomNumber = {
         this.makeNumbers();
         return {
             numbers  : this.numberStore,
-            winNums : [] 
+            winNums : [],
+            winRanking : 0
         };
     },
     clear : function() {
@@ -44,7 +46,7 @@ ready(function() {
     getJSON('get', 'js/win.json', function(data){
         winList = data;
     });
-    
+
     var button = document.querySelector('button');
     button.addEventListener('click', function(){
         var pickSize = document.querySelector('#pickSize').value;
@@ -56,6 +58,11 @@ ready(function() {
 });
 
 function start(pickSize) {
+    countWin.length = 45; 
+  
+    for(var i=0; i<countWin.length; i++){
+        countWin[i] = {winNum:i + 1, count:0};
+    } 
     for(var wi=0; wi<winList.length; wi++){
         var pickList = pick(pickSize);
         winList[wi].pickList = [];
@@ -67,32 +74,73 @@ function start(pickSize) {
 }
 
 function print(){
-    for(var i=0; i<winList.length; i++){
-        console.log(winList[i]); 
-    } 
+    countWin.sort(function(a, b){
+        return b.count - a.count;
+    });
+
+    countWin.length = 6;
+
+    countWin.sort(function(a, b){
+        return a.winNum - b.winNum;
+    });
+
+    console.log(countWin);
+
+    for(var i=winList.length-1; i>=0; i--) {
+        var round = winList[i].round;
+        var winNumber = numberString(winList[i].winNums);
+        var bonus = winList[i].bonusNum;
+        // console.log(round + ' ' + winNumber + ' Bonus ' + bonus);
+        // console.log(winList[i].pickList); 
+        console.log(winList[i]);
+    }
     // document.querySelector('.print').textContent = winList; 
+}
+
+function numberString(winNums) {
+    var winNumsString = '';
+    for(var i=0; i<winNums.length; i++){
+        if(winNums[i] < 10){
+            winNumsString += ' ' + winNums[i] + ' ';
+        } else {
+            winNumsString += winNums[i] + ' ';
+        }
+    }
+    return winNumsString; 
 }
 
 function compare(pick, win) {
     var pickNumbers = pick.numbers;
     var winNumbers = win.winNums; 
+    var bonus = win.bonusNum; 
+    var winRanking = 0; 
     pick.winNums = [];
     
-    for(var pi=0; pi<pickNumbers.length; pi++){
-        for(var wi=0; wi<winNumbers.length; wi++){
-            var pickNum = pickNumbers[pi];
-            var winNum = winNumbers[wi];
-            if(pickNum === winNum){
-                pick.winNums.push(pickNum);
-            }
-        }
-         
-    } 
-
-    // if(0 < pick.winNums.length){
-        win.pickList.push(pick);
-    // }
+    for(var pi=0; pi<pickNumbers.length; pi++) {
+        var pickNum = pickNumbers[pi];
+        if(-1 < winNumbers.indexOf(pickNum)){
+            pick.winNums.push(pickNum);
+            countWin[pickNum - 1].count++;
+        } 
+    }
+ 
+    switch(pick.winNums.length) {
+        case 3 : winRanking = 5; break;
+        case 4 : winRanking = 4; break;
+        case 5 : winRanking = 3; break;
+        case 6 : winRanking = 1; break;
+    }
     
+    if(pick.winNums.length === 5) {
+        if(-1 < pickNumbers.indexOf(bonus)){
+            pick.winNums.push(bonus);
+            countWin[bonus - 1].count++;
+            winRanking = 2;
+        } 
+    }
+
+    pick.winRanking = winRanking;
+    win.pickList.push(pick);
 }
 
 function ready(fn) {
