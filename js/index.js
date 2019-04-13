d1ff6603bd3aa7d49108b5c963bae54b7feb7e78
DevY <hanys1042@gmail.com>
2019-04-13 14:37:05 +0900

    총 당첨금액, 최고 당첨금맥 추가

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+var TAG_ROUND = '{round}';
+var TAG_WIN_NUMBERS = '{winNumbers}';
+var TAG_NUM_COLOR = ['{colorNum1}','{colorNum2}','{colorNum3}','{colorNum4}','{colorNum5}','{colorNum5}']; 
+var TAG_NUM = ['{num1}','{num2}','{num3}','{num4}','{num5}','{num6}']; 
+var TAG_BONUS = '{bonus}';
+var TAG_BONUS_COLOR = '{colorBonus}';
+var TAG_YOUR_TOTAL_MONEY = '{yourTotalMoney}'; 
+
+var TEMP_ROUND = '<div class="round close"><dl><dt>' + TAG_ROUND + '회</dt><dd><span class="num ' + TAG_NUM_COLOR[0] + '">' + TAG_NUM[0] + '</span><span class="num ' + TAG_NUM_COLOR[1] + '">' + TAG_NUM[1] + '</span><span class="num ' + TAG_NUM_COLOR[2] + '">' + TAG_NUM[2] + '</span><span class="num ' + TAG_NUM_COLOR[3] + '">' + TAG_NUM[3] + '</span><span class="num ' + TAG_NUM_COLOR[4] + '">' + TAG_NUM[4] + '</span><span class="num ' + TAG_NUM_COLOR[5] + '">' + TAG_NUM[5] + '</span> + <span class="num ' + TAG_BONUS_COLOR + '">' + TAG_BONUS + '</span></dd><dd class="money"><strong>' + TAG_YOUR_TOTAL_MONEY + '</strong><span>원</span></dd></dl></div>';
+
 // pick 최대 사이즈
 var MAX_SIZE = 100;
 
@@ -75,6 +85,12 @@ ready(function() {
             alert(MAX_SIZE_EXCESS_MSG); 
             return false;
         } 
+
+        var s = Math.abs(pickSize); 
+        if(!Number(pickSize)){
+            alert('1~100 사이의 정수를 입력하세요');
+        }
+
         start(pickSize);
     });
 });
@@ -91,62 +107,34 @@ function initCountWin() {
 function start(pickSize) {
     initCountWin();
 
+    winList.yourAllTotalMoney = 0;
+    winList.bestMoney = 0;
+    winList.bestRound = 0;
     for(var wi=0; wi<winList.length; wi++){
         var pickList = pick(pickSize);
         winList[wi].pickList = [];
         winList[wi].hasWin = false;
+        winList[wi].yourTotalMoney = 0; 
         for(var pi=0; pi<pickList.length; pi++){
             compare(pickList[pi], winList[wi]);
         }
+        winList.yourAllTotalMoney += winList[wi].yourTotalMoney;
     }
     
     sortCountWin();  
    
-    print(); 
-}
-
-// 결과 출력
-function print(){
-    for(var i=winList.length-1; i>=0; i--) {
-        var round = winList[i].round;
-        var winNumber = numberString(winList[i].winNums);
-        var bonus = winList[i].bonusNum;
-        console.log(winList[i]);
-    }
-}
-
-// 1~45 매칭 횟수 상위 6개만 남기고 절삭 후 번호 순서대로 정렬 
-function sortCountWin(){
-    countWin.sort(function(a, b){
-        return b.count - a.count;
-    });
-    countWin.length = 6;
-    countWin.sort(function(a, b){
-        return a.winNum - b.winNum;
-    });
-}
-
-// 길이가 6인 정수 배열을 문자열로 변환
-function numberString(winNums) {
-    var winNumsString = '';
-    for(var i=0; i<winNums.length; i++){
-        if(winNums[i] < 10){
-            winNumsString += ' ' + winNums[i] + ' ';
-        } else {
-            winNumsString += winNums[i] + ' ';
-        }
-    }
-    return winNumsString; 
+    print(false); 
 }
 
 // 길이가 6인 정수배열을 서로 비교하여 매칭되는 숫자를 수집
 function compare(pick, win) {
     var pickNumbers = pick.numbers;
-    var winNumbers = win.winNums; 
+    var winNumbers = win.winNumList; 
     var bonus = win.bonusNum; 
     var winRanking = 0; 
     pick.winNums = [];
-    
+    pick.yourMoney = 0;
+
     for(var pi=0; pi<pickNumbers.length; pi++) {
         var pickNum = pickNumbers[pi];
         if(-1 < winNumbers.indexOf(pickNum)){
@@ -172,11 +160,84 @@ function compare(pick, win) {
     }
 
     pick.winRanking = winRanking;
+    var moneyIndex = Number(pick.winRanking - 1);
+    if(-1 < moneyIndex){
+        var yourMoney = win.winMoney[moneyIndex]; 
+        win.yourTotalMoney += yourMoney;
+        pick.yourMoney = yourMoney;
+    }
+
+    if(winList.bestMoney < pick.yourMoney){
+        winList.bestMoney = pick.yourMoney;
+        winList.bestRound = win.round;
+    }
+
     if(win.hasWin === false && (0 < winRanking && winRanking)) {
         win.hasWin = true;
     } 
     
     win.pickList.push(pick);
+}
+
+// 1~45 매칭 횟수 상위 6개만 남기고 절삭 후 번호 순서대로 정렬 
+function sortCountWin(){
+    countWin.sort(function(a, b){
+        return b.count - a.count;
+    });
+    countWin.length = 6;
+    countWin.sort(function(a, b){
+        return a.winNum - b.winNum;
+    });
+}
+
+// 결과 출력
+function print(isAll){
+    var roundList = ''; 
+    for(var wi=winList.length-1; wi>=0; wi--) {
+        var round = winList[wi].round;
+        var bonus = winList[wi].bonusNum;
+        var yourTotalMoney = winList[wi].yourTotalMoney;
+
+        document.querySelector('.win-info .total strong').textContent = toWon(winList.yourAllTotalMoney);
+        document.querySelector('.win-info .best strong').textContent = winList.bestMoney;
+        document.querySelector('.win-info .best .best-round').textContent = winList.bestRound;
+        
+        var htmlRound = TEMP_ROUND.replace(TAG_ROUND,round);
+        for(var ni=0; ni<winList[wi].winNumList.length; ni++){
+            var num = winList[wi].winNumList[ni];
+            htmlRound = htmlRound.replace(TAG_NUM[ni],num).replace(TAG_NUM_COLOR[ni],numberColor(num));
+        }
+
+        htmlRound = htmlRound.replace(TAG_BONUS,bonus).replace(TAG_BONUS_COLOR,numberColor(bonus));
+        htmlRound = htmlRound.replace(TAG_YOUR_TOTAL_MONEY,yourTotalMoney);
+
+        if(winList[wi].hasWin === false){
+            htmlRound = htmlRound.replace('round','round no-win');
+        }else{
+            htmlRound = htmlRound.replace('round','round win');
+        }
+       
+        if(winList[wi].hasWin === false && isAll === false){
+            continue;
+        }
+
+        roundList += htmlRound;
+    }
+    document.querySelector('.wrap-result').innerHTML = '';
+    document.querySelector('.wrap-result').insertAdjacentHTML('afterBegin',roundList);
+}
+
+// 길이가 6인 정수 배열을 문자열로 변환
+function numberString(winNums) {
+    var winNumsString = '';
+    for(var i=0; i<winNums.length; i++){
+        if(winNums[i] < 10){
+            winNumsString += ' ' + winNums[i] + ' ';
+        } else {
+            winNumsString += winNums[i] + ' ';
+        }
+    }
+    return winNumsString; 
 }
 
 // $(document).ready();
@@ -225,4 +286,12 @@ function numberColor(num){
         case 4 : color = 'pink'; break;
     }
     return color;
+}
+
+function toWon(str){
+    str = str + '';
+    if(3 < str.length){
+        
+    }
+    return str;
 }
